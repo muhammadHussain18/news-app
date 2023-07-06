ec6bb706c39b74b8342888ff707884e841454516
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-07-06 06:48:20 -0700

    Add files via upload
    complete

---
+++ smit assignment 15 news app/aap.js
@@ -0,0 +1,88 @@
+let loader = document.getElementById("loader")
+let content = document.getElementById("content")
+let callNews = (search, page) => {
+    fetch(`https://api.worldnewsapi.com/search-news?api-key=467f6ec2212641998a1066d322368935&text=${search}&offset=${page ? page : 0}&number=12`)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            loader.style.display ="none"
+            content.style.display ="block"
+
+            const news = res.news
+            for (var i = 0; i < news.length; i++) {
+                const { image, title, text, publish_date } = news[i]
+                let getNews = document.getElementById("news")
+                getNews.innerHTML += `
+        <div class="card mt-2" style="width: 18rem;">
+                <img src="${image}" class="news-img card-img-top" alt="IMG">
+                   <div class="card-body card-div">
+                    <h5 class="card-title">${title.slice(0, 20)}...</h5>
+                    <h6 class="card-title date">Date : ${publish_date.slice(0, 10)}</h6>
+
+                    <p class="card-text text">${text.slice(0, 100)}...</p>
+               </div>
+            </div>  `
+
+            }
+
+        })
+        .catch(err => console.log(err))
+}
+
+callNews()
+let page = 0;
+
+let searchBtn = () => {
+    let search = document.getElementById("search")
+    let getNews = document.getElementById("news")
+    news.innerHTML = ""
+    loader.style.display ="flex"
+    content.style.display ="none"
+    callNews(search.value)
+
+}
+
+
+let loadMore = () => {
+    let search = document.getElementById("search")
+    page++
+    callNews(search.value, page)
+
+}
+
+
+let home = () =>{
+    let getNews = document.getElementById("news")
+    news.innerHTML = ""
+    loader.style.display ="flex"
+    content.style.display ="none"
+    callNews()
+}
+
+let pakistan = () => {
+    let getNews = document.getElementById("news")
+    news.innerHTML = ""
+    loader.style.display ="flex"
+    content.style.display ="none"
+    callNews("pakistan")
+
+
+}
+
+let currency = () => {
+    let getNews = document.getElementById("news")
+    news.innerHTML = ""
+    loader.style.display ="flex"
+    content.style.display ="none"
+    callNews("currency")
+
+}
+
+let sports = () => {
+    let getNews = document.getElementById("news")
+    news.innerHTML = ""
+    loader.style.display ="flex"
+    content.style.display ="none"
+    callNews("sports")
+
+}
