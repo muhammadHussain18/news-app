5977f61154d47c8f7ca6fb0d9ef72c250968b55e
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-07-07 04:44:17 +0500

    Add files via upload
    ADD MORE CSS

--- smit assignment 15 news app/aap.js
+++ smit assignment 15 news app/aap.js
@@ -1,27 +1,29 @@
 let loader = document.getElementById("loader")
 let content = document.getElementById("content")
 let callNews = (search, page) => {
-    fetch(`https://api.worldnewsapi.com/search-news?api-key=467f6ec2212641998a1066d322368935&text=${search}&offset=${page ? page : 0}&number=12`)
+    fetch(`https://api.worldnewsapi.com/search-news?api-key=682de8d5e3d344f485b5120e2c2ce008&text=${search}&offset=${page ? page : 0}&number=12`)
         .then(res => res.json())
         .then(res => {
             console.log(res)
-            loader.style.display ="none"
-            content.style.display ="block"
+            loader.style.display = "none"
+            content.style.display = "block"
 
             const news = res.news
             for (var i = 0; i < news.length; i++) {
                 const { image, title, text, publish_date } = news[i]
                 let getNews = document.getElementById("news")
+                
                 getNews.innerHTML += `
         <div class="card mt-2" style="width: 18rem;">
-                <img src="${image}" class="news-img card-img-top" alt="IMG">
+                <img src="${image}" class="news-img card-img-top" >
                    <div class="card-body card-div">
-                    <h5 class="card-title">${title.slice(0, 20)}...</h5>
+                    <h5 class="card-title">${title.slice(0, 30)}...</h5>
                     <h6 class="card-title date">Date : ${publish_date.slice(0, 10)}</h6>
 
                     <p class="card-text text">${text.slice(0, 100)}...</p>
                </div>
             </div>  `
+
 
             }
 
@@ -36,8 +38,8 @@ let searchBtn = () => {
     let search = document.getElementById("search")
     let getNews = document.getElementById("news")
     news.innerHTML = ""
-    loader.style.display ="flex"
-    content.style.display ="none"
+    loader.style.display = "flex"
+    content.style.display = "none"
     callNews(search.value)
 
 }
@@ -51,19 +53,19 @@ let loadMore = () => {
 }
 
 
-let home = () =>{
+let home = () => {
     let getNews = document.getElementById("news")
     news.innerHTML = ""
-    loader.style.display ="flex"
-    content.style.display ="none"
+    loader.style.display = "flex"
+    content.style.display = "none"
     callNews()
 }
 
 let pakistan = () => {
     let getNews = document.getElementById("news")
     news.innerHTML = ""
-    loader.style.display ="flex"
-    content.style.display ="none"
+    loader.style.display = "flex"
+    content.style.display = "none"
     callNews("pakistan")
 
 
@@ -72,8 +74,8 @@ let pakistan = () => {
 let currency = () => {
     let getNews = document.getElementById("news")
     news.innerHTML = ""
-    loader.style.display ="flex"
-    content.style.display ="none"
+    loader.style.display = "flex"
+    content.style.display = "none"
     callNews("currency")
 
 }
@@ -81,8 +83,8 @@ let currency = () => {
 let sports = () => {
     let getNews = document.getElementById("news")
     news.innerHTML = ""
-    loader.style.display ="flex"
-    content.style.display ="none"
+    loader.style.display = "flex"
+    content.style.display = "none"
     callNews("sports")
 
 }
